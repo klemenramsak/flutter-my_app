6273e00b462d9e2e905fc6deb397e8ed33d97477
klemenramsak <klemza1@gmail.com>
2022-03-28 16:16:59 +0200

    Made basic quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,23 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favourite colour?",
+      "What is your favourite animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("My first App"),
         ),
-        body: const Text("This is my default text"),
+        body: Column(
+          children: const [
+            Text("The question"),
+            ElevatedButton(onPressed: null, child: Text("Answer 1")),
+            ElevatedButton(onPressed: null, child: Text("Answer 2")),
+            ElevatedButton(onPressed: null, child: Text("Answer 3")),
+          ],
+        ),
       ),
     );
   }
