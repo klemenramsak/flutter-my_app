67591dd733b648c57b02e3c6a3a73ddd7ec6ff86
klemenramsak <klemza1@gmail.com>
2022-03-31 17:52:26 +0200

    Created private properties for file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,18 @@ class MyApp extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -36,13 +36,13 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Text(questions[_questionIndex]),
             ElevatedButton(
-                onPressed: answerQuestion, child: const Text("Answer 1")),
+                onPressed: _answerQuestion, child: const Text("Answer 1")),
             ElevatedButton(
-                onPressed: answerQuestion, child: const Text("Answer 2")),
+                onPressed: _answerQuestion, child: const Text("Answer 2")),
             ElevatedButton(
-                onPressed: answerQuestion, child: const Text("Answer 3")),
+                onPressed: _answerQuestion, child: const Text("Answer 3")),
           ],
         ),
       ),
